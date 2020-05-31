4ad4970d10219fb9b284062f52ca4db23e85d0dc
Najaaz Nabhan <najaaznabhan@gmail.com>
2020-05-31 09:14:18 +0530

    Front-end is nearly completed. Back-end is nearly done. Have a bug to fix - when you search a country and click it, it doesnt go the infowindow

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
 var map;
 var markers = [];
 var infoWindow;
+var found =[];
 
 function initMap() {
     var SL = {lat: 6.927079, lng: 80.7718};
     map = new google.maps.Map(document.getElementById('map'), {
         center: SL,
-        zoom: 4,
+        zoom: 3.5,
     });
     infoWindow = new google.maps.InfoWindow();
+    
+    var input = document.getElementById("country-code");
+    input.addEventListener("keyup", function(event){
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("myBtn").click();
+        }
+    });
+
     showMarkers()
+    setContent(statistics)
+    setContentListener()
 }
 
 
@@ -25,7 +37,9 @@ function createMarker(latlng, name, death , countryCode) {
         infoWindow.open(map, marker);
     });
     markers.push(marker);
-  
+
+    setContent(found)
+    setContentListener()
 }
 
 function showMarkers(){
@@ -41,30 +55,69 @@ function showMarkers(){
     })
 }
 
-function setContent(){
+function setContent(statistic){
     var content = ''
-    statistics.forEach(function(country){
+    statistic.forEach(function(country){
         content += `
-        <div class="info-list">
-            <div class="info-title">
-                <div class="info-title-name">Sri Lanka</div>
-            </div>
-            <div class="info-stats">
-                <div class="info-confirmed">
-                    <i class="fas fa-virus"></i>
-                    120,455
+        <div class="info">
+            <div class="info-list">
+                <div class="info-title">
+                    <div class="info-title-name">${country.country}</div>
                 </div>
-                <div class="info-recovered">
-                    <i class="fas fa-thumbs-up"></i>
-                    255,455
-                </div>
-                <div class="info-death">
-                    <i class="fas fa-skull"></i>
-                    1,247
+                <div class="info-stats">
+                    <div class="info-confirmed">
+                        <i class="fas fa-virus"></i>
+                        ${country.totalConfirmed}
+                    </div>
+                    <div class="info-recovered">
+                        <i class="fas fa-thumbs-up"></i>
+                        ${country.totalRecovered}
+                    </div>
+                    <div class="info-death">
+                        <i class="fas fa-skull"></i>
+                        ${country.totalDeaths}
+                    </div>
                 </div>
             </div>
         </div>`
 
+        document.querySelector(".country-container-list").innerHTML = content
+    })
+}
+
+function searchCountry(){
+    found = []
+    var search_items = document.querySelector("#country-code").value 
+    var search_item = search_items.toUpperCase()
+    if (search_item){
+        statistics.forEach(function(country){
+            var country_name = country.country
+            var country_uppper_name = country_name.toUpperCase()
+            if (search_item == country_uppper_name){
+                found.push(country)
+            }
+        })
+    }else{
+        setContent(statistics)
+        alert("PLEASE INPUT A ZIPCODE")
+    }
+
+    if (found){
+        setContent(found)
+        console.log(found)
+    }else{
+        console.log("NOT FOUND")
+    }
+}
+
+function setContentListener(){
+    infoWindow.close()
+    var selectedContent = document.querySelectorAll(".info")
+    console.log(selectedContent)
+    selectedContent.forEach(function(elem,index){
+        elem.addEventListener("click" , function(){
+            google.maps.event.trigger(markers[index] , "click")
+        })
     })
 }
 
