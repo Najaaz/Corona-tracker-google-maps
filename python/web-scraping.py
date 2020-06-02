a63c63cfa0a221ae9d80fe679f4c0d4c707b420f
Najaaz Nabhan <najaaznabhan@gmail.com>
2020-06-02 20:31:48 +0530

    Attempt no.1 to fix the screen issue in iphone6+

---
+++ python/web-scraping.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get("https://api.coronatracker.com/v3/stats/worldometer/topCountry")
+soup = BeautifulSoup(page.content , "html.parser")
+
+literal_text = f"let statistics = {soup}"
+
+info_file = open("js/info.js" , "w")
+info_file.write(literal_text)
+info_file.close()
+print(literal_text)
